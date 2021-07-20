401a14bb1e65fb57dafdc89d17c9676709473aee
lucaspiritogit <lucasignaciopirito@gmail.com>
2021-07-20 18:33:39 -0300

    0.1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,20 +7,23 @@ export default function Navbar() {
     return (
         <div className="navbar">
             <div className="navLogoName">
-                <img src={logo} alt="" className="logoImage" />
-                <h1>Company</h1>
-                <div className="navLinksResponsive">
-                    <img src={navBurger} alt="" className="navBurger" />
-                </div>
+                <a className="navHomeLink" href="/">
+                    <img src={logo} alt="" className="logoImage" />
+                    <h1>Company</h1>
+                    <div className="navLinksResponsive">
+                        <img src={navBurger} alt="" className="navBurger" />
+                    </div>
+                </a>
             </div>
             <div className="navLinksContainer">
                 <ul>
+                    <a href="/">
                     <button className="playButton">
-                        <a href="">Play</a>
-                    </button>
-                    <li><a href="">About Us</a></li>
-                    <li><a href="">Contact</a></li>
-                    <li><a href="">FAQ</a></li>
+                        Play 
+                    </button></a>
+                    <li><a href="/">About Us</a></li>
+                    <li><a href="/">Contact</a></li>
+                    <li><a href="/">FAQ</a></li>
                 </ul>
             </div>
         </div>

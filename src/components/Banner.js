094ba68b6f6536941959ade0654332863b34e515
lucaspiritogit <lucasignaciopirito@gmail.com>
2021-08-13 12:46:08 -0300

    fixed video banner width

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,14 +4,14 @@ import bannerVideo from '../videos/banner.mp4'
 export default function Banner() {
     return (
 
-            <div>
+
                 <div className="bannerContainer">
                     <video id="bannerVideo" src={bannerVideo} type='video/mp4'autoPlay loop muted></video>
                     <div className="bannerText">
                         <h1>Play, learn and enjoy <span>Chess</span></h1>
                     </div>
                 </div>
-            </div>
+
 
     )
 }
